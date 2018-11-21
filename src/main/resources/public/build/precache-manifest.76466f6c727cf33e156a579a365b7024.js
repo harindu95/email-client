792cf727e2dd1de4e096d5883940fa047a7f9b02
harindu95 <harindudilshan@gmail.com>
2018-11-20 19:54:30 -0700

    removed symlink

---
+++ src/main/resources/public/build/precache-manifest.76466f6c727cf33e156a579a365b7024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bf531d3aeef1c9d82960",
+    "url": "/static/js/main.bf531d3a.chunk.js"
+  },
+  {
+    "revision": "971c38c9d43def5bbb99",
+    "url": "/static/js/1.971c38c9.chunk.js"
+  },
+  {
+    "revision": "bf531d3aeef1c9d82960",
+    "url": "/static/css/main.c80fd652.chunk.css"
+  },
+  {
+    "revision": "4c3a9df1b6efe35f0ea5ca7aa3ab97f3",
+    "url": "/index.html"
+  }
+];
